6824f6f172942873496e46460701de04f6d60c3c
boku0301 <75344485+boku0301@users.noreply.github.com>
2020-12-02 12:35:29 +0900

    sdfg

--- Advanced/day05/해상도.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Toolkit;
-import java.awt.Dimension;
-  
-public class 해상도 {
-	public static void main(String[] agrs){
-		Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
-		System.out.println("해상도 : " + res.width + " x " + res.height);  
-	}
-}
